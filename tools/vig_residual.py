422c452c9ef17090cb626b834e00fc77c054647b
Jon Niehof <jtniehof@gmail.com>
2016-10-08 17:31:02 -0400

    vig_residual: include original, stretch (original and resid same brightness)

--- tools/vig_residual.py
+++ tools/vig_residual.py
@@ -34,6 +34,20 @@ for p in params:
         stdin=dcp.stdout,
         stderr=open(os.devnull, "w"))
     img = pgm.pgm_from_string(image_data)
+    outf = os.path.join('vignetting_' + d if d else 'vignetting',
+                        '{0}_{1}_orig.pgm'.format(f, ap))
+    minval, maxval = numpy.percentile(img, (1, 99))
+    orig = 65535 * \
+        ((numpy.require(img, dtype=numpy.float64) - minval) /
+         (maxval - minval))
+    orig[orig < 0] = 0
+    orig[orig > 65535] = 65535
+    orig = numpy.require(orig, requirements='CAO', dtype='>u2')
+    with open(outf, 'wb') as of:
+        of.write(b'P5\n')
+        of.write(b'{0} {1} {2}\n'.format(orig.shape[1], orig.shape[0],
+                                         65535))
+        orig.tofile(of)
     r = numpy.indices(img.shape, dtype=numpy.float32) + 0.5
     r[0, ...] -= img.shape[0] / 2.
     r[1, ...] -= img.shape[1] / 2.
@@ -41,13 +55,17 @@ for p in params:
         math.hypot(img.shape[0] / 2., img.shape[1] / 2.)
     img = img / (1 + params[p][0] * r ** 2 + params[p][1] * r ** 4 +
                  params[p][2] * r ** 6)
+    img = 65535 * \
+        ((numpy.require(img, dtype=numpy.float64) - minval) /
+         (maxval - minval))
+    img[img < 0] = 0
     img[img > 65535] = 65535
-    img = numpy.require(img, dtype=numpy.uint16)
+    img = numpy.require(img, requirements='CAO', dtype='>u2')
     outf = os.path.join('vignetting_' + d if d else 'vignetting',
                         '{0}_{1}_resid.pgm'.format(f, ap))
     with open(outf, 'wb') as f:
         f.write(b'P5\n')
         f.write(b'{0} {1} {2}\n'.format(img.shape[1], img.shape[0],
-                                        numpy.max(img)))
+                                        65535))
         img.tofile(f)
 
